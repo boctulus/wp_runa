ddda77ae1e1cfe44a5400cb2538a3d297e253457
Pablo Bozzolo <boctulus@gmail.com>
2023-04-20 17:32:56 +0800

    '.'

--- controllers/AjaxFormController.php
+++ controllers/AjaxFormController.php
@@ -9,65 +9,13 @@ use boctulus\SW\core\libs\Validator;
 
 class AjaxFormController
 {    
-    function save_form(){
+    function dummy(){
         $req  = Request::getInstance();
         $data = $req->as_array()->getBody();
 
-        if (empty($data)){
-            error("Falta el body o esta vacio o el formato es incorrecto", 400);
-        }
-    
-        $rules = [
-            'fname' 			=> ['type'=>'alpha','required'=>true,'min'=>3],
-            'lname' 			=> ['type'=>'alpha','required'=>true,'min'=>2,'max'=>100],
-            'email' 			=> ['type'=>'email','required'=>true], 
-            'input_channel_id'  => ['type'=>'int',  'required'=>true,'min'=>0],
-            'source_id'         => ['type'=>'int',  'required'=>true,'min'=>0],
-            'interest_type_id'  => ['type'=>'int',  'required'=>true,'min'=>0],
-            'project_id'        => ['type'=>'int',  'required'=>true,'min'=>0],
-            'extra_fields' 	    => ['type'=>'array','min'=>1],
-        ];
-    
-        $v = new Validator;
-    
-        if (!$v->validate($data, $rules)){
-           return error("Validation error", 400, $v->getErrors());
-        } 
-
-        // return response([
-        //     'data' => $data
-        // ]);
-        //////////////////////////////
-    
-        $cfg = config();
-
-        Logger::dump($data); //
-    
         try {
-
-            $client = ApiClient::instance()
-            ->setHeaders([
-                'Authorization: ' . $cfg['api_token'],
-                'Content-Type: application/json'
-            ]);
-
-            $client
-            ->disableSSL()
-            //->cache()
-            //->redirect()
-            ->setBody($data)
-            ->setUrl($cfg['api_url'])
-            ->post()
-            ->getResponse();
-
-            $status = $client->getStatus();
-
-            if ($status != 201 && $status != 200){
-                throw new \Exception($client->error());
-            }
-           
-            return response([
-                'data' => $client->data()
+            response([
+                'Hiiiiiii'
             ]);
 
         } catch (\Exception $e){
@@ -77,4 +25,73 @@ class AjaxFormController
             return error($err);
         }   
     }
+
+    // function save_form(){
+    //     $req  = Request::getInstance();
+    //     $data = $req->as_array()->getBody();
+
+    //     if (empty($data)){
+    //         error("Falta el body o esta vacio o el formato es incorrecto", 400);
+    //     }
+    
+    //     $rules = [
+    //         'fname' 			=> ['type'=>'alpha','required'=>true,'min'=>3],
+    //         'lname' 			=> ['type'=>'alpha','required'=>true,'min'=>2,'max'=>100],
+    //         'email' 			=> ['type'=>'email','required'=>true], 
+    //         'input_channel_id'  => ['type'=>'int',  'required'=>true,'min'=>0],
+    //         'source_id'         => ['type'=>'int',  'required'=>true,'min'=>0],
+    //         'interest_type_id'  => ['type'=>'int',  'required'=>true,'min'=>0],
+    //         'project_id'        => ['type'=>'int',  'required'=>true,'min'=>0],
+    //         'extra_fields' 	    => ['type'=>'array','min'=>1],
+    //     ];
+    
+    //     $v = new Validator;
+    
+    //     if (!$v->validate($data, $rules)){
+    //        return error("Validation error", 400, $v->getErrors());
+    //     } 
+
+    //     // return response([
+    //     //     'data' => $data
+    //     // ]);
+    //     //////////////////////////////
+    
+    //     $cfg = config();
+
+    //     Logger::dump($data); //
+    
+    //     try {
+
+    //         $client = ApiClient::instance()
+    //         ->setHeaders([
+    //             'Authorization: ' . $cfg['api_token'],
+    //             'Content-Type: application/json'
+    //         ]);
+
+    //         $client
+    //         ->disableSSL()
+    //         //->cache()
+    //         //->redirect()
+    //         ->setBody($data)
+    //         ->setUrl($cfg['api_url'])
+    //         ->post()
+    //         ->getResponse();
+
+    //         $status = $client->getStatus();
+
+    //         if ($status != 201 && $status != 200){
+    //             throw new \Exception($client->error());
+    //         }
+           
+    //         return response([
+    //             'data' => $client->data()
+    //         ]);
+
+    //     } catch (\Exception $e){
+    //         $err = "Error con el request " . $e->getMessage();
+    //         Logger::log($err);
+    
+    //         return error($err);
+    //     }   
+    // }
 }
