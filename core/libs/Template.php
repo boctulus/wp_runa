965717dd05677fd798f231ade6813cb0ac317cd1
Pablo Bozzolo <boctulus@gmail.com>
2023-04-23 14:11:26 +0800

    '.'

--- core/libs/Template.php
+++ core/libs/Template.php
@@ -7,6 +7,13 @@ use boctulus\SW\core\libs\Page;
 class Template
 {
     /*
+        Retorna tema actual
+    */
+    static function get(){
+        return get_option('template');
+    }
+
+    /*
         Cambia temporalmente el "theme" de WordPress 
 
         Ejemplo de uso:
@@ -30,10 +37,6 @@ class Template
 
     static function getDirectory(){
         return get_template_directory_uri();
-    }
-
-    static function getCurrentTheme(){
-        return get_option('template');
     }
 
     static function printName(){
